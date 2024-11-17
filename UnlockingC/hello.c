98e867b19e1ac43bb87a70f756ac0cc64cebb031
Rael <santosrafaelpro@gmail.com>
2024-11-16 23:31:40 -0300

    unlock C starting

---
+++ UnlockingC/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!");
+    printf("\tHello, World!");
+    printf("\n\tHello, World!");
+
+    printf("\n\t\tHello, World!");
+
+    return 0;
+}
