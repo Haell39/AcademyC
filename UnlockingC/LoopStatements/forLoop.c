58f65f4225ad5d54149555a0680ffddcf8f3311b
Rael <santosrafaelpro@gmail.com>
2024-11-21 19:33:26 -0300

    updating

---
+++ UnlockingC/LoopStatements/forLoop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () 
+{
+    printf("Wellcome\n"); // 100x times
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Wellcome!\n");
+    }
+    
+    
+
+
+    return 0;
+}
+
