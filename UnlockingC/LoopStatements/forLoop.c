4b1d555b957c2fce770fe625c4f69480ffe3b0ee
Rael <santosrafaelpro@gmail.com>
2024-11-25 18:27:42 -0300

    for loop C

--- UnlockingC/LoopStatements/forLoop.c
+++ UnlockingC/LoopStatements/forLoop.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
-int main () 
+int main()
 {
-    printf("Wellcome\n"); // 100x times
+    printf("Wellcome\n"); //
 
     for (int i = 0; i < 5; i++)
     {
         printf("Wellcome!\n");
     }
-    
-    
 
+    // Iterations showing
+
+    printf("\n");
+
+    for (int k = 0; k <= 5; k++)
+    {
+        printf("Just Testing 2 (Iteracao %d)\n", k);
+    }
+
+    // Tabuada
+
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d * 7 = %d\n", i, i*7);
+    }
+    
 
     return 0;
 }
-
